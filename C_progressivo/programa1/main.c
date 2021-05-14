58ef5a82de5313e535ac0afa18d113cdcec9dd4d
rodrigo98matos <rodrigo98matos@gmail.com>
2021-05-13 23:04:18 -0300

    c progressivo

--- C_progressivo/programa1/main.c
+++ C_progressivo/programa1/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void){
-    printf("\7Antes do \\t \t depois do \\t");
+
+int main()
+{   long int a;
+    printf("int : %d bytes\n", sizeof( int ) );
+    printf("short int: %d bytes\n", sizeof( short ) );
+    printf("long int: %d bytes\n", sizeof( a ) );
     return 0;
-};
+}
